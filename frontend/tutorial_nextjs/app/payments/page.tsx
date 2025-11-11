577b4d1d399a5c1238f152676138f5dba499558d
abhay-86 <abhaysinghpatel690@gmail.com>
2025-11-11 00:50:59 -0800

    Add Payment page and redirect utility; enhance feature utility documentation

--- frontend/tutorial_nextjs/app/payments/page.tsx
+++ frontend/tutorial_nextjs/app/payments/page.tsx
@@ -1,8 +1,30 @@
+"use client";
 
-export default function Home() {
+import { useSearchParams } from "next/navigation";
+
+export default function Payment() {
+  const searchParams = useSearchParams();
+
+  // Get query params
+  const feature = searchParams.get("feature");
+  const product = searchParams.get("product");
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white text-black dark:bg-black dark:text-white">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+    <div className="flex min-h-screen items-center justify-center bg-white text-black dark:bg-black dark:text-white flex-col space-y-4">
+      <h1 className="text-4xl font-bold">Payment Page</h1>
+
+      <div className="text-lg">
+        {feature && (
+          <p>
+            <span className="font-semibold">Feature:</span> {feature}
+          </p>
+        )}
+        {product && (
+          <p>
+            <span className="font-semibold">Product:</span> {product}
+          </p>
+        )}
+      </div>
     </div>
   );
 }
