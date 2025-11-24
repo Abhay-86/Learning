7deb41e531bf24f7c57c9cba84ddfa7e4eca1cc8
abhay-86 <abhaysinghpatel690@gmail.com>
2025-11-24 05:52:46 -0800

    moved the profile and settings

--- frontend/tutorial_nextjs/app/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function settingsPage() {
-  return (
-    <div>
-      <h1>Settings Page</h1>
-      <p>This is the settings page accessible to authorized users.</p>
-    </div>
-  );
-}
